d4fd3ed2bd8099b966398ec4e73772a50f181b4b
cfunch29 <cfunch29@gmail.com>
2026-01-06 18:12:19 -0500

    fixed code to include submenu. didn't have the right syntax for the links and forgot to add the function into the logic for submenu

--- scripts/script.js
+++ scripts/script.js
@@ -26,7 +26,7 @@ var menuLinks = [
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = 'var(--main-bg)';
 mainEl.innerHTML = '<h1>DOM Manipulation</h1>';
-mainEl.classList.add('flex-ctr')
+mainEl.classList.add('flex-ctr');
 
 // top menu
 const topMenuEl = document.getElementById('top-menu')
@@ -35,7 +35,7 @@ topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 
 topMenuEl.classList.add("flex-around");
 
-console.log(topMenuEl)
+// console.log(topMenuEl)
 
 // menu links
 for (let link of menuLinks) {
@@ -45,40 +45,87 @@ for (let link of menuLinks) {
   topMenuEl.appendChild(anchor);
 }
 
-// submenu
-const subMenuEl = document.getElementById('sub-menu')
-  subMenuEl.style.height = "100%";
-  subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
-  subMenuEl.style.position = 'absolute';
-  subMenuEl.style.top = '0';
-  subMenuEl.classList.add("flex-around");
+// submenu css styling
+const subMenuEl = document.getElementById('sub-menu');
+subMenuEl.style.height = "100%";
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
+subMenuEl.style.position = 'absolute';
+subMenuEl.style.top = '0';
+subMenuEl.classList.add("flex-around");
+// subMenuEl.style.border = '3px solid red'; - testing to see if subMenu is active
 
-  // console.log(subMenuEl)
+// console.log(subMenuEl)
 
-  // click top menu links
-const topMenuLinks = document.querySelectorAll("a");
+// click top menu links
+const topMenuLinks = topMenuEl.querySelectorAll("a");
 topMenuEl.addEventListener('click', function handleClick(clickEvent) {
   clickEvent.preventDefault();
   //  if clicked element is a link 
   if (clickEvent.target.tagName !== 'A')
     return;
 
-   console.log(clickEvent.target.textContent)
-    // add toggle for non-selected links
-    for (const a of topMenuLinks){
-          // toggle
+  // console.log(clickEvent.target.textContent)
+
+  // set submenu to hide and show based on action of clicking link that matches the data
+  const clickedLink = clickEvent.target;
+
+  // add toggle for non-selected links
+  for (const a of topMenuLinks) {
+    // toggle
     if (a !== clickEvent.target) {
       a.classList.remove('active');
     }
   }
-         clickEvent.target.classList.toggle('active');
+  clickEvent.target.classList.toggle('active');
 
-  topMenuEl.addEventListener('click', handleClick);
+  // find objects in menuLinks, call them linkObj
+  let clickedLinkObj = null;
+  for (const linkObj of menuLinks) {
+    //if it matches, store in memory
+    if (linkObj.text === clickedLink.textContent) {
+      clickedLinkObj = linkObj;
 
+      //then stop when a match is found
+      break;
+    }
+  }
+
+ console.log('clickedLinkObj:', clickedLinkObj);
+
+  //when the matching object (link) in submenu and has a submenu link, change the CSS to 100% and show the menu
+  if (clickedLink.classList.contains('active') && clickedLinkObj && clickedLinkObj.subLinks) {
+    subMenuEl.style.top = '100%';
+    buildSubmenu(clickedLinkObj.subLinks);
+  } 
+  else {
+    // if not do not show submenu
+    subMenuEl.style.top = '0';
+    subMenuEl.innerHTML = '';
+  }
 });
+
+// clear submenu links
+function buildSubmenu(subLinks) {
+  subMenuEl.innerHTML = '';
+  for (let subLink of subLinks) {
+    let subAnchor = document.createElement('a');
+    subAnchor.setAttribute('href', subLink.href);
+    subAnchor.textContent = subLink.text;
+    subMenuEl.appendChild(subAnchor);
+  }
+}
+
+
+
+
+
+topMenuEl.addEventListener('click', handleClick);
+
+
 console.log(topMenuEl)
 
 console.log(topMenuLinks)
 
 console.log(mainEl)
 console.log(menuLinks)
+
